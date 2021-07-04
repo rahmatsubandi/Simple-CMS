47d40e91f672b29b2269e708fe20cd9e2ccb188f
Rahmat Subandi <rhmtin12@gmail.com>
2021-07-04 22:49:53 +0700

    first commit

---
+++ application/views/_includes/public/footer.php
@@ -0,0 +1,39 @@
+</div>
+<footer class="footer footer-transparent d-print-none">
+    <div class="container">
+        <div class="row text-center align-items-center flex-row-reverse">
+            <div class="col-lg-auto ml-lg-auto">
+                <ul class="list-inline list-inline-dots mb-0">
+                    <li class="list-inline-item"><a href="<?= base_url() ?>" target="_blank" class="link-secondary">Simple CMS</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+                <ul class="list-inline list-inline-dots mb-0">
+                    <li class="list-inline-item">
+                        Copyright © <?= date('Y') ?>
+                        &bullet;
+                        Handcraft full <span class="text-danger">❤</span>
+                        <a href="<?= base_url() ?>" class="link-secondary">Reza</a>.
+                        All rights reserved.
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+</div>
+<!-- Libs JS -->
+<script src="<?= base_url(); ?>assets/dist/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url(); ?>assets/dist/libs/jquery/dist/jquery.slim.min.js"></script>
+<script src="<?= base_url(); ?>assets/dist/libs/apexcharts/dist/apexcharts.min.js"></script>
+<script src="<?= base_url(); ?>assets/dist/libs/jqvmap/dist/jquery.vmap.min.js"></script>
+<script src="<?= base_url(); ?>assets/dist/libs/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<!-- Tabler Core -->
+<script src="<?= base_url(); ?>assets/dist/js/tabler.min.js"></script>
+<script>
+    document.body.style.display = "block"
+</script>
+</body>
+
+</html>
---
+++ readme.rst
@@ -0,0 +1,6 @@
+# Buah Tangan Penuh cinta
+
+## akses admin
+
+username : admin
+password : admin
